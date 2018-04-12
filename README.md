ccd27e2f7ea9e88b6d6eecb33712aa229bea1252
Miles Elliott <mdelliot@ncsu.edu>
2018-04-12 12:27:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Block Attributes Glossary
+
+Create a block that will generate a glossary of all the blocks registered on your site with their available attributes. Very helpful when you are working with block templates.
+
+## `npm start` to build
+
+This project was bootstrapped with [Create Guten Block](https://github.com/ahmadawais/create-guten-block)
---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: GutenBlock Attributes Glossary
+ * Plugin URI: https://github.ncsu.edu/oitdesign/block-attributes
+ * Description: Adds a Block that displays a glossary of all Blocks and their Attributes
+ * Author: Miles Elliott, OIT Design
+ * Author URI: design.oit.ncsu.edu
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package CGB
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
